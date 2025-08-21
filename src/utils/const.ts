fc16093eaf0722a99decbd2bc09191bd1e935e23
evgenidefi <97796468+evgeniko@users.noreply.github.com>
2025-08-21 17:41:02 +0200

    new testing addr

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -17,16 +17,16 @@ export const TESTNET_SUI_MNEMONIC = "";
 
 export const TEST_NTT_TOKENS: NttContracts = {
   Solana: {
-    token: "8xLhmGP4yBN5V3YQJHXtuPXyr37bfqpJofijhCBa2wHQ",
-    manager: "nwiUea8Fw6gGZ2kSj564KWmvS8t5xDjz7FkahPVi7Vo",
+    token: "ANRdVy8fvhiJqXccFPpZijty5jQqkBp5Xjw4NXL1CsU1",
+    manager: "nasiB9hbB1s5ZWXAR54fjr3Y4HVTTzJMXNPAuedggQA",
     transceiver: {
-      wormhole: "77zEXrKfkNkZdj7LjZC9iGcfBhpcoPrvhDdEJjsq3geF",
+      wormhole: "Ba8FkdiKmNuGiwiZ6PVzkyR7uE2kMGV1aAYUh3XReLuA",
     },
     quoter: "Nqd6XqA8LbsCuG8MLWWuP865NV6jR1MbXeKxD4HLKDJ"
   },
   Sui: {
-    token: "0x261fd9b846984d607728733ab0cf019970caa9fbfef75ca8915762af964e190e::my_coin::MY_COIN",
-    manager: "0xa1b18a0f0791394aa7b271cad6bfc4bf592e554d34c6538359c5d282a559b815",
-    transceiver: { wormhole: "0x9fadc042b53ea139cef122b54f24843533c7a10200c2671628264c9d8b87e21b" },
+    token: "0x84a63e6b60767903c32b403fa6817fee6983470429e63bcf6da78a69ff00e4be::my_coin::MY_COIN",
+    manager: "0x8eb275808fe2f03ba24c9033de6322653262d3a360ee48541be6d6a072414306",
+    transceiver: { wormhole: "0x0540541acbd520955b0150b97c77e6c8b4ab3847fe8768b75551adcc3c5098ec" },
   },
 };
